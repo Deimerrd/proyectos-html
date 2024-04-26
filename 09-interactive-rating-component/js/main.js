86e7464b546d9fce293a1cc7c9617217f3b9e59d
Deimerd <deimerblandonmc@gmail.com>
2024-04-26 18:55:52 -0500

    ultimo retoque eliminacion de boton

--- 09-interactive-rating-component/js/main.js
+++ 09-interactive-rating-component/js/main.js
@@ -1,17 +1,12 @@
 const start = document.querySelector(".start");
 const Thankyou = document.querySelector(".Thank-you");
 const submitbtn = document.getElementById("submitbtn");
-const btnthank = document.getElementById("btn-rating");
+//const btnthank = document.getElementById("btn-rating");
 const rating = document.getElementById("rating");
 const rates = document.querySelectorAll(".btn");
 submitbtn.addEventListener("click", () => {
   Thankyou.classList.remove("windows");
   start.style.display = "none";
-});
-
-btnthank.addEventListener("click", () => {
-  Thankyou.classList.add("windows");
-  start.style.display = "block";
 });
 
 rates.forEach((rate) => {
